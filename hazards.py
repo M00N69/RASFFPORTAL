00355d2063047261187c1bb215eb73826e2f5050
MOON <133333114+M00N69@users.noreply.github.com>
2024-09-02 11:06:14 +0200

    Create hazards.py

---
+++ hazards.py
@@ -0,0 +1,18 @@
+hazards = [
+    "chlorpyrifos",
+    "chlorpyrifos-ethyl",
+    "Salmonella",
+    "Salmonella spp.",
+    "Salmonella Enteritidis",
+    "Aflatoxin",
+    "Aflatoxin B1",
+    "aflatoxin total",
+    "ochratoxin A",
+    "E220- sulfur dioxide",
+    "cadmium",
+    "Listeria monocytogenes",
+    "norovirus",
+    "peanut undeclared",
+    "gluten too high content",
+    # Ajouter d'autres dangers ici
+]
