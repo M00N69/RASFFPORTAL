48d2367b4f6aac67709b77d46cc011bcd88a527b
MOON <133333114+M00N69@users.noreply.github.com>
2024-11-13 21:52:01 +0100

    Update rasff.py

--- rasff.py
+++ rasff.py
@@ -5,6 +5,9 @@ import requests
 from io import BytesIO
 from datetime import datetime
 
+# Configure la page Streamlit (doit être exécuté en premier)
+st.set_page_config(page_title="RASFF Data Dashboard", layout="wide")
+
 # Main CSV data URL
 MAIN_DATA_URL = "https://raw.githubusercontent.com/M00N69/RASFFPORTAL/main/unified_rasff_data_with_grouping.csv"
 
